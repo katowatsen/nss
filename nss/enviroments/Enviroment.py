945e516d79577771e047a9b4653e313e5fd8216b
Kato-W <kato@watsen.net>
2019-10-30 20:54:07 -0400

    created abstract functions

---
+++ nss/enviroments/Enviroment.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Enviroment:
+
+    def __init__(self, sizeX, sizeY):
+        self.foodAmount = None
+        self.env = np.Array(sizeX, sizeY)
+
+    def setFood(self, amount):
+        #removeAllFood
+        pass
+
+    def removeFood(self, position):
+        pass
+
