53642431e6fc9e1a6f26fef16614b12c1a8fc430
Kato-W <kato@watsen.net>
2019-10-30 20:36:05 -0400

    added abstract functions

---
+++ nss/agents/Agent.py
@@ -0,0 +1,29 @@
+class Agent():
+
+    def __init__():
+        genome = {}
+        curEnergy = None
+        reqEnergy = None
+        fitness = None #might remove
+
+    def act():
+        #evaluate decissions based on genome and probability
+        pass
+
+    def search():
+        pass
+
+    def travel():
+        pass
+
+    def eatFood():
+        pass
+
+    def cooperate():
+        pass
+
+    def defect():
+        pass
+
+    def moralize():
+        pass
