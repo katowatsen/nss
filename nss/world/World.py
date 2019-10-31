9289030f227c8383a35c1f31fb2bcbeebe774b83
Kato-W <kato@watsen.net>
2019-10-30 20:53:23 -0400

    created abstract functions

---
+++ nss/world/World.py
@@ -0,0 +1,12 @@
+class World():
+
+    def __init__(self, totalCycles, ):
+        self.tick = None
+        self.cycle = None 
+        self.totalTicks = None
+        self.totalCycles = None
+
+    def updateTick(self):
+        pass
+
+
