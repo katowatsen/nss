a9169a63c052fd55c6f2aeb5e1ad6833460b0e9a
Kato-W <kato@watsen.net>
2019-10-30 20:53:42 -0400

    created abstract funcitons

---
+++ nss/engines/Engine.py
@@ -0,0 +1,14 @@
+class Engine():
+
+    def __init__(self):
+        pass
+
+    def run(self, agents, env, world):
+        pass
+
+    def outputToFile(self, IOfile):
+        pass
+
+
+
+            
