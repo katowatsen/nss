254d37d7aab260bf3c1b0659ea1e123d18cbca63
Kato-W <kato@watsen.net>
2019-10-31 08:38:49 -0400

    created ability to intalize and remove all food

--- nss/enviroments/Enviroment.py
+++ nss/enviroments/Enviroment.py
@@ -2,14 +2,29 @@ import numpy as np
 
 class Enviroment:
 
-    def __init__(self, sizeX, sizeY):
+    def __init__(self, dimensions):
         self.foodAmount = None
-        self.env = np.Array(sizeX, sizeY)
+        self.env = np.array(np.zeros(dimensions))
 
-    def setFood(self, amount):
-        #removeAllFood
+    def setFood(self):
+
+        self.removeAllFood()
+        i = 0
+
+        while(i < self.foodAmount):
+            randCoord = getRandomCoord()
+            self.env[randCoord[0], randCoord[1]] += 1
+            i+= 1
+
+
+    def getRandomCoord(self) 
+        return (np.random(0, self.env.shape[0]),
+                np.random(0, self.env.shape[1]))
+
+    def removeFoodAtAgent(self, position):
         pass
 
-    def removeFood(self, position):
-        pass
+    def removeAllFood(self):
+        self.env = np.array(np.zeros(dimensions))
+
 
