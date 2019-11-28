403d7c26a05f9677bb07e911112391427f41d628
Kato-W <kato@watsen.net>
2019-11-28 11:35:01 -0500

    add ability to increase cycle and ticks

--- nss/world/World.py
+++ nss/world/World.py
@@ -1,12 +1,13 @@
 class World():
 
-    def __init__(self, totalCycles, ):
-        self.tick = None
-        self.cycle = None 
-        self.totalTicks = None
-        self.totalCycles = None
+    def __init__(self, totalCycles, totalTicks):
+        self.tick = 0
+        self.cycle = 0
+        self.totalTicks = totalTicks
+        self.totalCycles = totalCycles
 
     def updateTick(self):
-        pass
+        self.tick += 1
 
-
+    def updateCycle(self):
+        self.cycle = 0
