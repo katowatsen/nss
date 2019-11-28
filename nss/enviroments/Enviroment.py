162c5a134866781ab295b60fa482a290f0aac54a
Kato-W <kato@watsen.net>
2019-11-27 19:55:08 -0500

    add init varibles modified functionsaccordingly

--- nss/enviroments/Enviroment.py
+++ nss/enviroments/Enviroment.py
@@ -1,30 +1,31 @@
 import numpy as np
+np.random.seed(0)
 
 class Enviroment:
 
-    def __init__(self, dimensions):
-        self.foodAmount = None
-        self.env = np.array(np.zeros(dimensions))
+    def __init__(self, dimensions, foodAmount):
+        self.dimensions = dimensions
+        self.foodAmount = foodAmount
+        self.env = np.array(np.zeros(self.dimensions))
 
     def setFood(self):
-
-        self.removeAllFood()
         i = 0
+        self.removeAllFood()
 
         while(i < self.foodAmount):
-            randCoord = getRandomCoord()
+            randCoord = self.getRandomCoord()
             self.env[randCoord[0], randCoord[1]] += 1
             i+= 1
 
 
-    def getRandomCoord(self) 
-        return (np.random(0, self.env.shape[0]),
-                np.random(0, self.env.shape[1]))
+    def getRandomCoord(self): 
+        return (np.random.randint(0, self.dimensions[0]),
+                np.random.randint(0, self.dimensions[1]))
 
-    def removeFoodAtAgent(self, position):
+    def removeFoodAtAgent(self, agent):
         pass
 
     def removeAllFood(self):
-        self.env = np.array(np.zeros(dimensions))
+        self.env = np.array(np.zeros(self.dimensions))
 
 
