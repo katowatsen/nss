4576f93d5f98473866cc194263772056ff5a0caf
Kato-W <kato@watsen.net>
2019-11-28 11:47:21 -0500

    add simple loops for interations

--- nss/engines/Engine.py
+++ nss/engines/Engine.py
@@ -3,12 +3,17 @@ class Engine():
     def __init__(self):
         pass
 
-    def run(self, agents, env, world):
-        pass
+    def run(self, agent_list, env, world):
+        while world.cycle <= world.totalCycles:
+            while world.tick <= word.totalTicks:
+                for agent in agent_list:
+                    agent.act()
+
+                agent.update()
+                env.update()
+                world.tick += 1
+            world.cycle += 1
 
     def outputToFile(self, IOfile):
         pass
 
-
-
-            
