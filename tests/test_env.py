ac025b35b85ca002e723bb912203b8e6e83cefe7
Kato-W <kato@watsen.net>
2019-11-28 12:18:46 -0500

    add random seed for each test

--- tests/test_env.py
+++ tests/test_env.py
@@ -2,10 +2,12 @@ import numpy as np
 from ..nss.enviroments.Enviroment import Enviroment as env
 
 def test_setFood():
+    np.random.seed(0)
     test_enviroment = env([2,2], 2)
     test_enviroment.setFood()
     assert np.array_equal(test_enviroment.env,  np.array([[0,1],[1,0]]))
 
 def test_removeAllFood():
+    np.random.seed(0)
     test_enviroment = env([2,2], 2)
     assert np.array_equal(test_enviroment.env,  np.array([[0,0],[0,0]]))
