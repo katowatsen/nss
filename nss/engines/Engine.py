69e6b068a2b0c491699c48e8194a4a1b1d37e2ae
Kato-W <kato@watsen.net>
2019-12-08 15:39:40 -0500

    fix bug with parallel processing

--- nss/engines/Engine.py
+++ nss/engines/Engine.py
@@ -1,5 +1,5 @@
 from multiprocessing import Pool
-from functools import partial
+from itertools import chain 
 
 class Engine():
 
@@ -7,21 +7,24 @@ class Engine():
         pass
 
     def run(self, agent_list, env, world):
+        pool = Pool(4)
 
         while world.cycle <= world.totalCycles:
             env.setFood()
 
             while world.tick <= world.totalTicks:
 
-
-
-                pool = Pool(4)
                 result = pool.map(self.worker, ((agent, env, world, agent_list) for agent in agent_list))
+                if isinstance(result[0], list):
+                    agent_list = list(chain.from_iterable(result))
+
+                else:
+                    agent_list = result 
+                #print(agent_list)
+
 
                 #for agent in agent_list:
                 #    agent.act(env,world, agent_list)
-                pool.close()
-                pool.join()
 
                 world.updateTick()
 
@@ -29,7 +32,9 @@ class Engine():
             world.resetTick()
             world.updateCycle()
 
-        return agent_list 
+        pool.close()
+        pool.join()
+        return agent_list
 
     def worker(self, arg):
         agent, env, world, agent_list = arg
