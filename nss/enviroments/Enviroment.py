fe4489e31d9b228abd83c9709eb752e89343a640
Kato-W <kato@watsen.net>
2019-12-07 21:38:36 -0500

    modified

--- nss/enviroments/Enviroment.py
+++ nss/enviroments/Enviroment.py
@@ -2,17 +2,16 @@ import numpy as np
 
 class Enviroment:
 
-    def __init__(self, dimensions, foodAmount):
-        self.dim = dimensions
+    def __init__(self, dim, foodAmount):
+
+        self.dim = dim #dim should be in row, col format
         self.foodAmount = foodAmount
         self.map = np.array(np.zeros(self.dim))
         self.foodValue = 500 #temp
 
-    def get(self, coord):
-        '''Returns element in array with row, col -> x, y indexing.'''
-        return self.map[coord[1]][coord[0]]
 
     def setFood(self):
+        '''Clears present food and sets food in the enviroment.'''
         i = 0
         self.removeAllFood()
 
@@ -26,19 +25,25 @@ class Enviroment:
         """Calculates a random coordinate based on the array's dimensions
         and returns a tuple of the random coordinate"""
 
-        return (np.random.randint(0, self.dim[1]),
-np.random.randint(0, self.dim[0]))
+        return (np.random.randint(0, self.dim[0]),
+np.random.randint(0, self.dim[1]))
                 
     def foodAtAgent(self, agent):
+        '''Determins if food is located at the agent.'''
+
         if self.map[int(agent.position[0]), int(agent.position[1])]:
             return True
         else:
             return False
 
     def removeFoodAtAgent(self, agent):
+        '''Removes food at the agent's position'''
+
         self.map[agent.position[0],agent.position[1]] -= 1
 
     def removeAllFood(self):
+        '''Removes all food in the enviroment.'''
+
         self.map = np.array(np.zeros(self.dim))
 
 
