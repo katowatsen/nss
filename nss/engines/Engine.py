2fd3757e5117578d2eaeed55a045362fc2ed0291
Kato-W <kato@watsen.net>
2019-12-08 14:02:01 -0500

    add currently non funcitonal parrael processing capability modified cycling system

--- nss/engines/Engine.py
+++ nss/engines/Engine.py
@@ -1,3 +1,6 @@
+from multiprocessing import Pool
+from functools import partial
+
 class Engine():
 
     def __init__(self):
@@ -6,17 +9,31 @@ class Engine():
     def run(self, agent_list, env, world):
 
         while world.cycle <= world.totalCycles:
+            env.setFood()
+
             while world.tick <= world.totalTicks:
-                for agent in agent_list:
-                    agent.act(env,world, agent_list)
+
+
+
+                pool = Pool(4)
+                result = pool.map(self.worker, ((agent, env, world, agent_list) for agent in agent_list))
+
+                #for agent in agent_list:
+                #    agent.act(env,world, agent_list)
+                pool.close()
+                pool.join()
 
                 world.updateTick()
 
 
-            world.tick = 0
+            world.resetTick()
             world.updateCycle()
 
         return agent_list 
+
+    def worker(self, arg):
+        agent, env, world, agent_list = arg
+        return agent.act(env, world, agent_list)
 
     def outputToFile(self, IOfile):
         pass
