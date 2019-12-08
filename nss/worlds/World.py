030c5a354cbf020c9db8c279790dcf81d4365dae
Kato-W <kato@watsen.net>
2019-12-08 14:00:23 -0500

    add reset tick function

--- nss/worlds/World.py
+++ nss/worlds/World.py
@@ -1,10 +1,13 @@
 class World():
 
     def __init__(self, totalCycles, totalTicks):
-        self.cycle = 0
-        self.tick = 0
+        self.cycle = 1 
+        self.tick = 1
         self.totalCycles = totalCycles
         self.totalTicks = totalTicks
+
+    def resetTick(self):
+        self.tick = 1
 
     def updateTick(self):
         self.tick += 1
