327f71b716b888718b1b31610ac6dbc9679a7215
Kato-W <kato@watsen.net>
2019-12-07 22:28:31 -0500

    replaced array enviroment with dictionary envrioment

--- nss/enviroments/Enviroment.py
+++ nss/enviroments/Enviroment.py
@@ -4,50 +4,45 @@ class Enviroment:
 
     def __init__(self, dim, foodAmount):
 
-        self.dim = dim #dim should be in row, col format
+        self.dim = dim #should be in (x, y) format
         self.foodAmount = foodAmount
-        self.map = np.array(np.zeros(self.dim))
-        self.foodValue = 500 #temp
+        self.map = {}
+        self.foodValue = 50
 
 
     def setFood(self):
-        '''Clears present food and sets food in the enviroment.'''
-        i = 0
+        
         self.removeAllFood()
 
-        while(i < self.foodAmount):
+        for i in range(0, self.foodAmount):
             randCoord = self.getRandomCoord()
-            self.map[randCoord[0], randCoord[1]] += 1
-            i+= 1
+
+            if randCoord in self.map:
+                self.map[randCoord] += 1
+            else:
+                self.map[randCoord] = 1
 
 
-    def getRandomCoord(self): 
-        """Calculates a random coordinate based on the array's dimensions
-        and returns a tuple of the random coordinate"""
+    def getRandomCoord(self):
+        """Calculates a random coordinate based on self.dim 
+        dimensions and returns a tuple of the random coordinate"""
 
         return (np.random.randint(0, self.dim[0]),
-np.random.randint(0, self.dim[1]))
-                
-    def foodAtAgent(self, agent):
-        '''Determins if food is located at the agent.'''
+                np.random.randint(0, self.dim[1]))
 
-        if self.map[int(agent.position[0]), int(agent.position[1])]:
+    def foodAtPosition(self, position):
+
+        if self.map.get(position) >= 1:
             return True
         else:
             return False
 
-    def removeFoodAtAgent(self, agent):
-        '''Removes food at the agent's position'''
+    def removeFoodAtPosition(self, position):
 
-        self.map[agent.position[0],agent.position[1]] -= 1
+        self.map[position] -= 1
 
     def removeAllFood(self):
-        '''Removes all food in the enviroment.'''
 
-        self.map = np.array(np.zeros(self.dim))
-
-
-
-
+        self.map.clear()
 
 
