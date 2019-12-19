5dd6eaa7c5432d1be9f3415161fcc8ab3f9fa0d8
Kato-W <kato@watsen.net>
2019-12-18 22:34:38 -0500

    made pytest to test genome bug

---
+++ tests/test_agent_genome.py
@@ -0,0 +1,34 @@
+from ..nss.enviroments.Enviroment import Enviroment
+from ..nss.agents.Agent import Agent
+from ..nss.worlds.World import World 
+import itertools
+import time 
+import copy
+import numpy as np
+
+def test_genome():
+    np.random.seed(0)
+    env = Enviroment((100,100),60)
+
+    agent = Agent(env, 10)
+
+    nrg = copy.copy(agent.reqEnergy)
+
+    initial_genome = agent.genome.copy()
+
+    agent.curEnergy = 1000000
+    agent.reproduce(env, 1000, 10, 1)
+    agent.determine_next(env, 1000, [agent])
+    agent.eatFood(env)
+    agent.curEnergy = 1000000
+    reproduced = agent.reproduce(env, 1000, 10, 1)
+    reproduced.append(agent)
+
+    agent = reproduced[-1]
+
+    print(agent.genome)
+    print(agent.reqEnergy)
+
+    
+
+    assert nrg == agent.reqEnergy
