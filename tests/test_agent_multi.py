d9b5858ba872667043599f45a6f288e5628592cb
Kato-W <kato@watsen.net>
2019-12-22 12:54:52 -0500

    modified test to work with reworked agent. add test to test the process time of a multiproccesses vs single processed engine

--- tests/test_agent_multi.py
+++ tests/test_agent_multi.py
@@ -1,5 +1,7 @@
 from ..nss.enviroments.Enviroment import Enviroment
 from ..nss.agents.Agent import Agent
+from ..nss.worlds.World import World 
+import time 
 from itertools import chain
 
 import numpy as np
@@ -9,17 +11,49 @@ def test_reproduce():
     agent_list = [Agent(env, 2) for i in range(0,1)]
     agent_list[0].curEnergy = 10
     agent_list[0].reqEnergy = 2
-    reproduced = agent_list[0].reproduce(2, env, 2, 0.01)
+    reproduced = agent_list[0].reproduce(env,2, 2, 0.01)
 
     for agent in reproduced:
-        agent_list.append(reproduced)
+        agent_list.append(agent)
 
-
-    assert (agent_list[0].curEnergy == agent_list[0].reqEnergy) == (len(agent_list) == 5)
+    assert agent_list[0].curEnergy == agent_list[0].reqEnergy and (len(agent_list) == 5) and id(agent.genome) != id(agent_list[1].genome)
 
 def test_mutateChild():
     env = Enviroment((5,5),2)
-    np.random.seed(0)
+    np.random.seed(10)
     t_agent = Agent(env, 2)
 
-    assert t_agent.genome != t_agent.mutateChild(t_agent, 0.1) 
+    assert t_agent.genome != t_agent.mutateChild(Agent(env, 2), 0.1)
+
+def test_timings():
+    np.random.seed(0)
+
+    env = Enviroment((100,100), 1)
+    env.setFood()
+    world = World(10,10)
+    agent = Agent(env, 2)
+
+    agent_list = [agent]
+
+
+    start_search = time.time()
+    agent.determine_next(env, world, agent_list)
+    end_search = time.time()
+
+    start_travel = time.time()
+    agent.update_strat(env, world, agent_list)
+    end_travel = time.time()
+
+    start2 = time.time()
+    agent.search(env)
+    end2 = time.time()
+
+
+    search_time = end_search - start_search
+    travel_time = end_travel - start_travel
+    time2 = end2-start2
+
+
+    assert search_time != travel_time 
+
+
