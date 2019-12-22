7ca2ef353147e664b21e20d9f75313047d079054
Kato-W <kato@watsen.net>
2019-12-22 12:48:25 -0500

    reworked acting of agent

--- nss/agents/Agent.py
+++ nss/agents/Agent.py
@@ -1,18 +1,20 @@
 import math
 import numpy as np
+import copy
 
 class Agent():
 
-    def __init__(self, env, MAX_mass):
+    def __init__(self, env, world, MAX_mass):
+        self.MAX_mass = MAX_mass
         #initalizes a genome with random genes
         self.genome = {
                   #MAX search length is the diagonal of the enviroment
-                  "search":np.random.random_sample() * math.hypot(
-                                env.dim[0], env.dim[1]),
+                  "search": np.random.random_sample() * math.hypot(
+                                env.dim[0], env.dim[1]) / world.totalTicks,
 
                   #MAX travel distance is the diagonal of the enviroment
                   "travel": np.random.random_sample() * math.hypot(
-                                env.dim[0], env.dim[1]),
+                                env.dim[0], env.dim[1]) /world.totalTicks,
 
                   #MAX mass is a predefined constatnt
                   "mass": np.random.random_sample()* MAX_mass,
@@ -58,11 +60,11 @@ class Agent():
             self.eatFood(env)
 
         if world.tick == world.totalTicks:
-            sub_agent_list = self.reproduce(env, 500, 10, 0.1)
+            sub_agent_list = self.reproduce(env,world, 10, self.MAX_mass, 5)
             '''kills current agent if it does not have
             required energy at the end of cycle'''
             
-            if self.curEnergy >= self.reqEnergy:
+            if self.curEnergy >= self.reqEnergy and self.reqEnergy > 0:
                 sub_agent_list.insert(0, self)
 
             return sub_agent_list
@@ -99,7 +101,7 @@ class Agent():
             self.position[1] += self.genome["travel"]
 
         else:
-            distance = nextPosition[1]
+            distance = self.genome["travel"] 
             theta = math.atan2(
                     (nextPosition[0][1] - self.position[1]),
                     (nextPosition[0][0] - self.position[0]))
@@ -114,21 +116,30 @@ class Agent():
             env.removeFoodAtPosition(self.position)
             self.curEnergy += env.foodValue
 
-    def reproduce(self, env, reproduceCost, MAX_mass, MAX_deviation):
+    def reproduce(self, env, world, reproduceCost, MAX_mass, MAX_deviation):
         sub_agent_list = []
 
-        while self.curEnergy >= self.reqEnergy + reproduceCost:
-            sub_agent_list.append(self.mutateChild(Agent(env, MAX_mass), MAX_deviation))
+        while self.curEnergy >= self.reqEnergy + reproduceCost and self.reqEnergy > 0:
+            child = Agent(env,world, MAX_mass)
+            sub_agent_list.append(self.mutateChild(child, MAX_deviation))
+
             self.curEnergy -= reproduceCost
 
         return sub_agent_list
 
-    def mutateChild(self,child, MAX_deviation):
-        child.genome = self.genome.copy()
+    def mutateChild(self, child, MAX_deviation):
 
+        child.genome.clear()
+        child.genome = copy.deepcopy(self.genome)
         for k in child.genome.keys():
+
             child.genome[k] += np.random.choice(
-            a = [-1,1]) *  np.random.random_sample() * MAX_deviation
+            a = [-1,1]) * np.random.random_sample() * MAX_deviation
+
+            if child.genome[k] < 0:
+                child.genome[k] = np.random.random_sample()*MAX_deviation
+
+        child.reqEnergy = child.genome["mass"] * 0.5 *math.pow(child.genome["travel"], 2) + child.genome["search"]
 
         return child
 
