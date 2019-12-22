1db92edf92189de9d06b288698d222eeab0c443c
Kato-W <kato@watsen.net>
2019-12-22 12:51:51 -0500

    add status bar and outputs to csv file

--- nss/engines/Engine.py
+++ nss/engines/Engine.py
@@ -1,5 +1,9 @@
 import multiprocessing as mp
 from itertools import chain 
+import copy
+#import os
+from tqdm import tqdm
+from ..util.Analysis import Analysis 
 
 class Engine():
 
@@ -7,39 +11,97 @@ class Engine():
         pass
 
     def run(self, agent_list, env, world):
+        print("Running nss:")
+
+        data = []
+
         pool = mp.Pool(mp.cpu_count())
+        
+        pbar = tqdm(total = world.totalCycles)
 
         while world.cycle <= world.totalCycles:
+            env.removeAllFood()
             env.setFood()
+            #print(len(agent_list))
+
+            avg = 0
+
+            if len(agent_list) != 0:
+
+                avg_genome = agent_list[0].genome.fromkeys(agent_list[0].genome, 0)
+
+            for agent in agent_list:
+                agent.curEnergy = 0
+                avg += agent.reqEnergy
+                for k in agent.genome.keys():
+
+                    avg_genome[k] += agent.genome[k]
+
+            if len(agent_list) != 0:
+                avg = avg / len(agent_list)
+                for k in agent.genome.keys():
+
+                    avg_genome[k] += agent.genome[k] 
+
+            else:
+                avg = "N/A"
+
+            for key in avg_genome.keys():
+                avg_genome[key] = avg_genome[key] / len(agent_list)
+                
+
+            data.append({"cycle": world.cycle,
+                         "length": len(agent_list), 
+                         "reqEnergy": avg,
+                         "avgSearch": avg_genome['search'],
+                         "avgTravel": avg_genome['travel'],
+                         "avgMass": avg_genome['mass'],
+                         "avgAltruism": avg_genome['altruism']
+                         }) 
 
             while world.tick <= world.totalTicks:
+                #print(world.tick)
 
-                result = pool.map(self.worker, ((agent, env, world, agent_list) for agent in agent_list))
-                if isinstance(result[0], list):
-                    agent_list = list(chain.from_iterable(result))
+                agent_list = pool.map(self.worker_determine_next, ((agent, env, world, agent_list) for agent in agent_list))
+
+                #update 
+                compiled_list = []
+                for agent in agent_list:
+                    compiled_list.append(agent.update_strat(env, world, agent_list))
+
+                #tests if complied list has multiple dimentions
+                if len(compiled_list) > 0 and isinstance(
+                       compiled_list[0], list):
+
+                    #flattens compiled list and assigns it to agent_list
+                    agent_list = list(chain.from_iterable(compiled_list))
 
                 else:
-                    agent_list = result 
-                #print(agent_list)
-
-
-                #for agent in agent_list:
-                #    agent.act(env,world, agent_list)
+                    #creates a shallow copy of compiled_list
+                    agent_list = compiled_list.copy()
 
                 world.updateTick()
 
 
+
             world.resetTick()
+            pbar.update(1)
             world.updateCycle()
 
         pool.close()
         pool.join()
+        pbar.close()
+
+
+        analyze = Analysis()
+
+        analyze.write(data)
+        analyze.read()
+
         return agent_list
 
-    def worker(self, arg):
+    def worker_determine_next(self, arg):
         agent, env, world, agent_list = arg
-        return agent.act(env, world, agent_list)
+        return agent.determine_next(env, world, agent_list)
 
-    def outputToFile(self, IOfile):
-        pass
 
