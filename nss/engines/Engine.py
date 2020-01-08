1342055e660a54121cc0dfa52018e68584cf6c7c
Kato-W <kato@watsen.net>
2020-01-08 17:24:48 -0500

    modified engine to work with other modules

--- nss/engines/Engine.py
+++ nss/engines/Engine.py
@@ -4,6 +4,7 @@ import copy
 #import os
 from tqdm import tqdm
 from ..util.Analysis import Analysis 
+import time
 
 class Engine():
 
@@ -12,6 +13,7 @@ class Engine():
 
     def run(self, agent_list, env, world):
         print("Running nss:")
+        analyze = Analysis()
 
         data = []
 
@@ -19,11 +21,17 @@ class Engine():
         
         pbar = tqdm(total = world.totalCycles)
 
+
         while world.cycle <= world.totalCycles:
+
+            #does things before the cycle begins
+
+            #prepares enviroment
             env.removeAllFood()
             env.setFood()
             env.foodPool = 0
 
+            #averages genome attributes of the agents
             avg = 0
 
             if len(agent_list) != 0:
@@ -50,6 +58,7 @@ class Engine():
                 avg_genome[key] = avg_genome[key] / len(agent_list)
                 
 
+            #prepares data for output 
             data.append({"cycle": world.cycle,
                          "length": len(agent_list), 
                          "reqEnergy": avg,
@@ -58,6 +67,12 @@ class Engine():
                          "avgMass": avg_genome['mass'],
                          "avgAltruism": avg_genome['altruism']
                          }) 
+
+            if world.cycle == 1:
+                analyze.writeHeaders(data)
+
+
+
 
             #pairs interaction partners
 
@@ -70,51 +85,57 @@ class Engine():
                 else:
 
                     agent.partner = copy.copy(
-                        agent_list[i].interact(world, env))
+                        agent_list[i].communicate(agent_list[i].determineAltruism()))
 
                 i += 1
 
             while world.tick <= world.totalTicks:
 
+                #agents interact with each other
+
                 agent_list = pool.map(self.worker_determine_next, ((agent, env, world, agent_list) for agent in agent_list))
+                
+                if world.tick == world.totalTicks:
+                    #add food to foodPool
+                    for agent in agent_list:
+                        agent.shareFood(env, agent.determineAltruism())
 
-                #add food to foodPool
-
-                for agent in agent_list:
-                    agent.interact(world, env)
                 #update 
                 compiled_list = []
 
                 for agent in agent_list:
                     compiled_list.append(agent.update_strat(env, world, agent_list))
 
-
                 #tests if complied list has multiple dimentions
                 if len(compiled_list) > 0 and isinstance(
                        compiled_list[0], list):
 
                     #flattens compiled list and assigns it to agent_list
-                    agent_list = list(chain.from_iterable(compiled_list))
+                    if world.tick == world.totalTicks:
+                        #removes agents
+                        compiled_list.append(world.removeAgents(agent_list))
+                        agent_list = list(chain.from_iterable(compiled_list)) 
 
                 else:
-                    #creates a shallow copy of compiled_list
-                    agent_list = compiled_list.copy()
+                    compiled_list = world.removeAgents(compiled_list)
+                    agent_list = copy.copy(compiled_list)
 
                 world.updateTick()
 
 
+            analyze.writeRow(data)
+            data.clear()
+
             world.resetTick()
-            pbar.update(1)
             world.updateCycle()
+            pbar.update(1)
 
         pool.close()
         pool.join()
         pbar.close()
 
 
-        analyze = Analysis()
 
-        analyze.write(data)
         analyze.read()
 
         return agent_list
