c86a20b7857e97c3cd996396760dbf6c5dcd3c62
Kato-W <kato@watsen.net>
2020-01-21 15:56:35 -0600

    gave up on cython implementation. reverted to regular python

--- run.py
+++ run.py
@@ -1,26 +1,7 @@
-from nss.enviroments import Enviroment 
-from nss.agents import Agent
-from nss.engines import Engine
-from nss.engines import EngineSingle
-from nss.worlds import World
-import numpy as np
-from time import time
-import copy
+from nss import core 
 
 def run():
-    np.random.seed(100)
-
-    engine = Engine.Engine()
-    world = World.World(10000,4)
-
-    env = Enviroment.Enviroment((100,100), 60)
-    env.foodValue = 90 
-    env.setFood()
-
-    agent_list = [Agent.Agent(env, world, 100) for i in range(0,100)]
-    pre_agent_list_len = len(agent_list)
-
-    agent_list = engine.run(agent_list, env, world)
+    core.start()
 
 if __name__ == '__main__':
     run()
